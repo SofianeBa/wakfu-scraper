44bfd124342aea26853ce4ce23f654a3263d53f0
SofianeBa <sofianebabouri9@gmail.com>
2023-01-22 21:13:54 +0100

    Version getting banned

--- py/json_compiler.py
+++ py/json_compiler.py
@@ -6,28 +6,45 @@ import time
 from fake_useragent import UserAgent
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+from progress.bar import ChargingBar
+import threading
 
+#options = Options()
+#ua = UserAgent()
+#userAgent = ua.random
+#print(userAgent)
+#options.add_argument(f'user-agent={userAgent}')
+#options.add_argument('--headless')
+##options.add_argument('--disable-extensions')
+#options.add_argument('--disable-gpu')
+#options.add_argument('--no-sandbox')
+#options.add_argument('disable-infobars')
+#options.add_argument('--disable-dev-shm-usage')
 
+#service = Service(r'C:\Users\sofia\Downloads\chromedriver_win32\chromedriver.exe')
+#service.start()
+#browser = webdriver.Chrome(options=options, service=service)
+
+#url = "https://www.wakfu.com/fr/mmorpg/encyclopedie/armures/"
+#monstres = tree.xpath("//div[@class='ak-container ak-content-list ak-displaymode-image-col']/div[@class='row ak-container']/div[@class='ak-column ak-container col-xs-12 col-md-6']/div/div/div/div[@class='ak-content']/div/a/span/text()")
+#pourcentages = tree.xpath("//div[@class='ak-container ak-content-list ak-displaymode-image-col']/div[@class='row ak-container']/div/div/div/div/div[@class='ak-aside']/text()")
 
 options = Options()
 ua = UserAgent()
-userAgent = ua.random
-print(userAgent)
-options.add_argument(f'user-agent={userAgent}')
-options.add_argument('--headless')
-options.add_argument('--disable-extensions')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
-options.add_argument('disable-infobars')
-options.add_argument('--disable-dev-shm-usage')
+
+def update_user_agent():
+    while True:
+        global options
+        userAgent = ua.random
+        options.add_argument(f'user-agent={userAgent}')
+        time.sleep(10)
+
+threading.Thread(target=update_user_agent).start()
 
 service = Service(r'C:\Users\sofia\Downloads\chromedriver_win32\chromedriver.exe')
 service.start()
 browser = webdriver.Chrome(options=options, service=service)
 url = "https://www.wakfu.com/fr/mmorpg/encyclopedie/armures/"
-#monstres = tree.xpath("//div[@class='ak-container ak-content-list ak-displaymode-image-col']/div[@class='row ak-container']/div[@class='ak-column ak-container col-xs-12 col-md-6']/div/div/div/div[@class='ak-content']/div/a/span/text()")
-#pourcentages = tree.xpath("//div[@class='ak-container ak-content-list ak-displaymode-image-col']/div[@class='row ak-container']/div/div/div/div/div[@class='ak-aside']/text()")
-
 #print(driver.page_source)
 #driver.quit()
 
@@ -196,29 +213,81 @@ def find_item_name_scraper(item_id):
     name = tree.xpath("//div[@class='container ak-main-container']/div[@class='ak-main-content']/div[@class='ak-main-page']/div/main/div[@class='ak-container ak-main-center']/div/div[@class='ak-title-container ak-backlink']/h1/text()")[1].strip()
     return name
 
-item["ID"] = items[1001]["definition"]["item"]["id"]
-item["Nom"] = items[1001]["title"]["fr"]
-item["Niveau"] = items[1001]["definition"]["item"]["level"]
-item["Type"] = find_item_type(items[1001])
-item["Rareté"] = rarity[items[1001]["definition"]["item"]["baseParameters"]["rarity"]]
-item["Description"] = items[1001]["description"]["fr"]
-item["Effet"] = find_effects(items[1001])
+def find_weapon_effects(item):
+    basic_effects = []
+    crit_effect = []
+    effets = {}
+    for value in item["definition"]["useEffects"]:
+        params = value["effect"]["definition"]["params"]
+        description = find_effect_desc(value["effect"]["definition"]["actionId"])
+        decoded_description = subprocess.run(["node", "./Decoder/decoder.js", description, json.dumps(params),str(item["definition"]["item"]["level"])], capture_output=True, text=True, encoding='utf-8')
 
+        if (description != "Inconnu"):
+            effect = {}
+            effect["ID"] = value["effect"]["definition"]["actionId"]
+            effect["Paramètres"] = params
+            effect["Effet"] = decoded_description.stdout.strip()
+            effect["areaShape"] = value["effect"]["definition"]["areaShape"]
+            effect["areaSize"] = value["effect"]["definition"]["areaSize"]
+            basic_effects.append(effect)
 
-id_recette = find_recipe_id(item["ID"])
-if id_recette < 0:
-    item["Recette"] = None
-else:
-    item["Recette"] = {}
-    item["Recette"]["ID"] = id_recette
-    item["Recette"]["Métier"] = find_recipe_job(id_recette)
-    item["Recette"]["Ingrédients"] = find_recipe_ingredients(id_recette)
+    for value in item["definition"]["useCriticalEffects"]:
+        params = value["effect"]["definition"]["params"]
+        description = find_effect_desc(value["effect"]["definition"]["actionId"])
+        decoded_description = subprocess.run(["node", "./Decoder/decoder.js", description, json.dumps(params),str(item["definition"]["item"]["level"])], capture_output=True, text=True, encoding='utf-8')
 
-item["Drop"] = find_drop(item["ID"])
+        if (description != "Inconnu"):
+            effect = {}
+            effect["ID"] = value["effect"]["definition"]["actionId"]
+            effect["Paramètres"] = params
+            effect["Effet"] = decoded_description.stdout.strip()
+            effect["areaShape"] = value["effect"]["definition"]["areaShape"]
+            effect["areaSize"] = value["effect"]["definition"]["areaSize"]
+            crit_effect.append(effect)
+    effets["Effet basique"] = basic_effects
+    effets["Effet critique"] = crit_effect
+    if(effets):
+        return effets
+    return "Inconnu"
 
+new_items = []
+bar = ChargingBar('En cours', max=100)
 
+for x in range(0,350):
+    item = {}
+    item["ID"] = items[x]["definition"]["item"]["id"]
+    item["Nom"] = items[x]["title"]["fr"]
+    item["Niveau"] = items[x]["definition"]["item"]["level"]
+    item["Type"] = find_item_type(items[x])
+    item["Rareté"] = rarity[items[x]["definition"]["item"]["baseParameters"]["rarity"]]
+    item["Description"] = items[x]["description"]["fr"]
+    item["Effet Equipé"] = find_effects(items[x])
+
+    id_recette = find_recipe_id(item["ID"])
+    if id_recette < 0:
+        item["Recette"] = None
+    else:
+        item["Recette"] = {}
+        item["Recette"]["ID"] = id_recette
+        item["Recette"]["Métier"] = find_recipe_job(id_recette)
+        item["Recette"]["Ingrédients"] = find_recipe_ingredients(id_recette)
+
+    item["Drop"] = find_drop(item["ID"])
+
+    item["Effet Arme"] = find_weapon_effects(items[x])
+    new_items.append(item)
+    bar.next()
 
 browser.quit()
-print(item)
+bar.finish()
+#with open(r"./result/result.json", "w",encoding='utf-8') as file:
+    #file.write(html_source)
+
+with open('./result/data.json', mode='w', encoding='utf-8') as f:
+    json.dump([], f)
+
+with open('./result/data.json', mode='w', encoding='utf-8') as f:
+    json.dump(new_items, f)
+
 
 print("--- %s seconds ---" % (time.time() - start_time))
